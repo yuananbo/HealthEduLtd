9c2aec25067c616839490badd359ae7c60f37be4
Ruoxin <ruoxinlei068@gmail.com>
2026-03-21 18:21:48 -0400

    feat: Patient Can Rate Therapist After Completed Appointment

--- backend/controllers/therapist/common.controller.js
+++ backend/controllers/therapist/common.controller.js
@@ -2,51 +2,55 @@ import Therapist from "../../models/therapist.model.js";
 import CommonService from "../../services/common.service.js";
 
 export const getTherapistRatings = async (req, res) => {
-    const therapistId = req.params._id;
-  
+  const therapistId = req.params.id;
+
   try {
-    const therapist = await Therapist.findById(therapistId).populate('therapistratings');
+    const therapist = await CommonService.getTherapistRatings(therapistId);
+    const averageRating = CommonService.calculateAverageRating(therapist.ratings);
+
     if (!therapist) {
-      return res.status(404).json({ message: 'Therapist not found' });
+      return res.status(404).json({ message: "Therapist not found" });
     }
-    res.json(therapist);
+
+    res.json({ therapist, averageRating });
   } catch (error) {
-    console.error('Error fetching therapist with ratings:', error);
-    res.status(500).json({ message: 'Server error' });
+    console.error("Error fetching therapist with ratings:", error);
+    res.status(500).json({ message: "Server error" });
   }
-}
+};
 
-export const addRating =  async(req, res) => {
-    const {rating, review} = req.body;
-    const therapistId = req.params.id;
-    const patientId = req.user._id
+export const addRating = async (req, res) => {
+  const { rating, review } = req.body;
+  const therapistId = req.params.id;
+  const patientId = req.user._id;
 
-    try {
-
-        const newRating = await CommonService.addTherapistRating(patientId, therapistId, rating, review)
-        res.status(201).json({success:"success", rating:newRating})
-
-    }catch(err){
-        console.log("Error adding rating:", err)
-        res.status(500).json({message: 'Server error'})
-    }
-}
+  try {
+    const newRating = await CommonService.addTherapistRating(
+      patientId,
+      therapistId,
+      rating,
+      review
+    );
+    res.status(201).json({ success: "success", rating: newRating });
+  } catch (err) {
+    console.log("Error adding rating:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 
 
 // get ratings
 export const getTherapistProfileWithRatings = async (req, res) => {
-    const therapistId = req.user._id;
-  
-    try {
-      const therapist = await CommonService.getTherapistRatings(therapistId);
-      const averageRating = CommonService.calculateAverageRating(therapist.ratings);
-  
-      res.json({ therapist, averageRating})
-}catch(err){
-    console.error('Error fetching therapist profile:', error);
-    res.status(500).json({ message: 'Server error' });
+  const therapistId = req.user._id;
+
+  try {
+    const therapist = await CommonService.getTherapistRatings(therapistId);
+    const averageRating = CommonService.calculateAverageRating(therapist.ratings);
+
+    res.json({ therapist, averageRating });
+  } catch (err) {
+    console.error("Error fetching therapist profile:", err);
+    res.status(500).json({ message: "Server error" });
   }
-
-}
-
+};
